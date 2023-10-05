51571a7b804d9b8068d30cd7094087cbac014ab2
Aakriti Goyal <82526001+AakritiGoyal12458@users.noreply.github.com>
2023-10-05 11:18:21 +0530

    Create ReferenceParameter.cs

---
+++ ReferenceParameter.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        // Method with two reference parameters
+        static void disp(ref int p1, ref int p2)
+        {
+            p1 = p1 + 10;
+            p2 = p2 + 10;
+        }
+
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 20;
+
+            Console.WriteLine("Before Method Calls:");
+            Console.WriteLine("x: " + x);
+            Console.WriteLine("y: " + y);
+
+            // Call the disp method with two reference parameters
+            disp(ref x, ref y);
+
+            Console.WriteLine("After Method Calls:");
+            Console.WriteLine("x (after disp): " + x); // x is modified
+            Console.WriteLine("y (after disp): " + y); // y is modified
+
+           
+        }
+    }
+}
+
+Before Method Calls:
+x: 10
+y: 20
+After Method Calls:
+x (after disp): 20
+y (after disp): 30
